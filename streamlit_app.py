2877bd90c243139a72dbb25ca4c878f5f6cd7870
pushpender-1299 <125812660+pushpender-1299@users.noreply.github.com>
2023-02-21 15:43:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,4 +53,4 @@ add_my_fruit = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', add_my_fruit)
 
 
-
+my_cur.execute("insert into fruit_load_list values('from streamlit')")
